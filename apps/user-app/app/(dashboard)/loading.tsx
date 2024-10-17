edfab8ce4823eb4311cbb46f831cdb0c5f9b28f2
ThriloknathVemula <thriloknath15vemula@gmail.com>
2024-10-17 22:00:19 +0530

    loaders

---
+++ apps/user-app/app/(dashboard)/loading.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {Circles} from "react-loader-spinner"
+
+export default function Loading(){
+    return <div className="flex justify-center items-center h-[100vh] w-screen"><Circles
+    height="80"
+    width="80"
+    color="#00308F"
+    ariaLabel="circles-loading"
+    wrapperStyle={{}}
+    wrapperClass=""
+    visible={true}
+    /></div>
+}
